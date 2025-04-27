49fd9d977a4caa94e01bdd13fb89e21bc1f40bfc
VIVEKKOYANI <vivek.koyani@gmail.com>
2025-04-27 22:41:58 +0530

    ArrayWrapper code add

---
+++ arrayWrapper/index.js
@@ -0,0 +1,54 @@
+// Leetcode
+
+/**
+ * @param {number[]} nums
+ * @return {void}
+ */
+var ArrayWrapper = function(nums) {
+  this.nums = nums
+};
+
+/**
+* @return {number}
+*/
+ArrayWrapper.prototype.valueOf = function() {
+  return this.nums.reduce((sum, num) => sum + num, 0);
+}
+
+/**
+* @return {string}
+*/
+ArrayWrapper.prototype.toString = function() {
+  return `[${this.nums.join(',')}]`;
+}
+
+/**
+* const obj1 = new ArrayWrapper([1,2]);
+* const obj2 = new ArrayWrapper([3,4]);
+* obj1 + obj2; // 10
+* String(obj1); // "[1,2]"
+* String(obj2); // "[3,4]"
+*/
+
+// es6
+
+class ArrayWrapper {
+  constructor(nums) {
+      this.nums = nums;
+  }
+
+  valueOf() {
+      return this.nums.reduce((sum, num) => sum + num, 0);
+  }
+
+  toString() {
+      return `[${this.nums.join(",")}]`;
+  }
+}
+
+const obj1 = new ArrayWrapper([1,2]);
+const obj2 = new ArrayWrapper([3,4]);
+
+console.log(obj1 + obj2);    // 10
+console.log(String(obj1));   // "[1,2]"
+console.log(String(obj2));   // "[3,4]"
