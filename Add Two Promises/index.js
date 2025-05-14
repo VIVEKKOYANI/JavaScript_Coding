b942b633c1fd68ec288e88c8576e7cab1ed1a4ae
VIVEKKOYANI <vivek.koyani@gmail.com>
2025-05-14 09:58:50 +0530

    add two promises

---
+++ Add Two Promises/index.js
@@ -0,0 +1,7 @@
+var addTwoPromises = async function(promise1, promise2) {
+    const [val1, val2] = await Promise.all([promise1, promise2]);
+    return val1 + val2;
+};
+
+addTwoPromises(Promise.resolve(2), Promise.resolve(2))
+  .then(console.log); // Output: 4
